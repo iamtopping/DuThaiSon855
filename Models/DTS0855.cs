c013e5ef9374ff7e49dc3003af2efd19438e539d
ADMIN <ADMIN@THAISON>
2021-12-15 14:09:41 +0700

    Commit_Create_Table_DTS0855

--- Models/DTS0855.cs
+++ Models/DTS0855.cs
@@ -9,8 +9,9 @@ namespace DuThaiSon855.Models
         [Key]
          [StringLength(20), Display(Name= " Dư Thái Sơn ID")]
         public string DTSID { get; set; }
-        [StringLength(50), Display(Name= "Tên Dư Thái Sơn ")]
+        [StringLength(50), Display(Name= "Tên Dư Thái Sơn "),Required]
         public string DTSName { get; set; }
+         [ Display(Name= " Dư Thái Sơn Giới Tính "),Required]
         public string DTSGender { get; set; }
               
     }
