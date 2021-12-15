61b6c41ea8ecfd99398ae1fe7468966d163faae1
ADMIN <ADMIN@THAISON>
2021-12-15 13:23:56 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DuThaiSon855.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
